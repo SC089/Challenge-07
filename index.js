dc279db13b4b60ab39d3ecdc2dfcf2cd5574f24e
Sebastian C <sacasadevall@gmail.com>
2024-10-11 16:07:54 -0400

    might've accidentally overwritten the gitignore file, but installed inquirer, imported inquirer and fs to the index.js. Also made some questions to prompt the user.

---
+++ index.js
@@ -0,0 +1,45 @@
+// TODO: Include packages needed for this application
+import inquirer from "inquirer";
+import fs from 'fs';
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'What is the name of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a brief description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation steps for your project?'
+    },
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'How do you use this project?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How can others contribute to your project?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'How can users test your project?'
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
